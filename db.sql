ffc8cdbcc9871fa31e24877381b7c90cb249f8b6
hekmatsuha353@gmail.com <you@example.com>
2022-08-05 14:54:39 +0430

    OOPLoginRegister

---
+++ db.sql
@@ -0,0 +1,57 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+CREATE DATABASE IF NOT EXISTS `lr` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `lr`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `salt` varchar(32) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `joined` datetime NOT NULL,
+  `group` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `permissions` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
+(1, 'Standard User', ''),
+(2, 'Administrator', '{\r\n"admin": 1,\r\n"moderator" : 1\r\n}');
+
+-- --------------------------------------------------------
+
+
+CREATE TABLE IF NOT EXISTS `users_session` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hash` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
